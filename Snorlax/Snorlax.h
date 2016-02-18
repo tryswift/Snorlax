730c670d0e4d7604163573358e4571c15a3501be
Jeff Hui <jeff@jeffhui.net>
2016-02-17 19:29:29 -0800

    Initial Commit - Via Xcode
    This is the first commit that Xcode created when creating the project.
    Nothing much else here in this commit.

---
+++ Snorlax/Snorlax.h
@@ -0,0 +1,19 @@
+//
+//  Snorlax.h
+//  Snorlax
+//
+//  Created by Jeff Hui on 2/17/16.
+//  Copyright © 2016 Jeff Hui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Snorlax.
+FOUNDATION_EXPORT double SnorlaxVersionNumber;
+
+//! Project version string for Snorlax.
+FOUNDATION_EXPORT const unsigned char SnorlaxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Snorlax/PublicHeader.h>
+
+
