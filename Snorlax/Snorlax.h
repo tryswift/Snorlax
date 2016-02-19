16e73aed1e07e0c4dc41cbf6140bea47e6340281
Jeff Hui <jeff@jeffhui.net>
2016-02-18 19:54:17 -0800

    Add OS X Scheme and Target
    Clean up Swift.h header
    
    Here we did the same thing as renaming the iOS target: change the
    PRODUCT_NAME to Snorlax. All the custom files the target template added
    could be removed.
    
    Although it's definitely possible to unify the Info.plists between the
    two targets, we've left it duplicated for now.

--- Snorlax/Snorlax.h
+++ Snorlax/Snorlax.h
@@ -1,19 +1,7 @@
-//
-//  Snorlax.h
-//  Snorlax
-//
-//  Created by Jeff Hui on 2/17/16.
-//  Copyright © 2016 Jeff Hui. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
+@import Foundation;
 
 //! Project version number for Snorlax.
 FOUNDATION_EXPORT double SnorlaxVersionNumber;
 
 //! Project version string for Snorlax.
 FOUNDATION_EXPORT const unsigned char SnorlaxVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Snorlax/PublicHeader.h>
-
-
