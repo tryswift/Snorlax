79796cabc11e8fa52a8739164561c8014e1cbb28
Jeff Hui <jeff@jeffhui.net>
2016-02-21 05:27:06 -0800

    Add PromiseKit as a dependency
    Add to Cartfile for carthage users.
    Cloned via submodule for manual integration users.
    Add dependency to podspec for cocoapod users.
    
    Updated code + tests to use PromiseKit (why not?)

--- Tests/Snorlax/SnorlaxTests.swift
+++ Tests/Snorlax/SnorlaxTests.swift
@@ -4,6 +4,7 @@ import Foundation
 
 #if !SWIFT_PACKAGE
     import Nimble
+    import PromiseKit
 #endif
 
 #if os(OSX) || os(iOS) || os(watchOS) || os(tvOS)
@@ -34,14 +35,24 @@ class SnorlaxTests: XCTestCase, XCTestCaseProvider {
 
     // Xcode Only: SPM doesn't seem to support building testDependencies
     //             and PromiseKit doesn't support SPM.
-
     func testRest() {
         let start = NSDate()
-        rest()
+        let promise = rest()
         let end = NSDate()
         let duration = end.timeIntervalSinceDate(start)
         expect(duration) >= 0
         expect(duration) < 2
+
+        var promisedInterval: NSTimeInterval?
+        waitUntil(timeout: 5) { done in
+            promise.then { (interval) -> NSTimeInterval in
+                promisedInterval = interval
+                return interval
+            }.always(done)
+        }
+
+        expect(promisedInterval) >= 0
+        expect(promisedInterval) < 2
     }
     #endif
 }
